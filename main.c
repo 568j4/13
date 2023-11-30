381082cc3281d8c8275f24759857da9e82822e27
568j4 <144990819+568j4@users.noreply.github.com>
2023-11-30 15:39:39 +0900

    1.

--- main.c
+++ main.c
@@ -9,20 +9,20 @@ struct student{
 };
 
 int main(int argc, char *argv[]) {
-	
-	 struct student s1= {123, "JUYEOP", 4.3};
-	 
+
+	struct student s1= {123, "JUYEOP", 4.3};
+
 	s1.ID=123456;
 	s1.name[0]='c';
 	s1.score= 0.7;
-	
+
 	printf("ID : %i\n", s1.ID);
 	printf("name : %s\n", s1.name);
 	printf("grade : %f\n", s1.score);
-	
+
 	strcpy(s1.name, "Bongjun");
 	printf("name2 : %s\n");
-	 
+
 	system ("PAUSE");
 	return 0;
 }
